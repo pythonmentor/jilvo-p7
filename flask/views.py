3836ed8df4da2dccb28c4d9c44e01edaaf2f989e
unknown <jmillot41@gmail.com>
2020-06-01 16:16:44 +0200

    Set the map, receving Json and get information of the route

--- flask/views.py
+++ flask/views.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template,jsonify,make_response
 from flask_cors import CORS
+from flask import request as req
 import requests
 import json
+from parse_question import parsing
 
 app = Flask(__name__)
 CORS(app=app)
@@ -10,16 +12,36 @@ CORS(app=app)
 def index():
     return render_template('/index.html')
 
+@app.route('/map')
+def map():
+    return render_template('/Untitled-1.html')
 
-@app.route('/api_google')
+
+
+@app.route('/api_google/')
 def grandpy():
-    return_data = requests.get("https://world.openfoodfacts.org/api/v0/product/737628064502.json")
+    user_raw_text = req.args.get('user_raw_text')
+    list_question = parsing(user_raw_text)
+    adress = "+".join(list_question)
+    return_data = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address="+ adress +",&key=AIzaSyAKfLjoRy19P25S1KUOorpwBJ-psQ5oRg4")
     return_data = return_data.json()
-    
+    for geometry in return_data["results"]:
+        if geometry.get("geometry",False):
+            # print(geometry["geometry"])
+            dict_lat_lng={}
+            dict_lat_lng=geometry["geometry"]
+            latitude = dict_lat_lng['location']["lat"]
+            longitude = dict_lat_lng['location']["lng"]
+            dict_return={}
+            dict_return["latitude"] = latitude
+            dict_return["longitude"] = longitude
+            print(dict_return)
+            
     response = app.response_class(
         response=json.dumps(return_data, ensure_ascii=False), status=200, mimetype="application/json"
     )
-    return response
+    dict_return["response"] = response
+    return dict_return
 
 if __name__ == "__main__":
     app.run(debug=False)
--- tests/test_pybot_google.py
+++ tests/test_pybot_google.py
@@ -1,17 +1,24 @@
-import pytest
 import requests
 import json
 
 def test_OK_google_places():
     """Test if the function returns a dict with the needed values."""
-    def json():
-        """Return a fake json result from request."""
-        location = {"lat": 20, "lng": 30}
-        geometry = {"location": location}
-        candidates = [{"formatted_address": "champ de Mars",
-                       "geometry": geometry,
-                       "name": "Tour Eiffel"}]
-        response = {"candidates": candidates, "status": "OK"}
-        return response
+    return_data = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=tour+eiffel,&key=AIzaSyAKfLjoRy19P25S1KUOorpwBJ-psQ5oRg4")
+    return_data = return_data.json()
+    for geometry in return_data["results"]:
+        if geometry.get("geometry",False):
+            # print(geometry["geometry"])
+            dico={}
+            dico=geometry["geometry"]
+            lat = dico['location']["lat"]
+            lng = dico['location']["lng"]
+            print('lat is' + str(lat) + 'lng is' + str(lng))
+        # for location in geometry["geometry"]:
+        #     if location.get("location",False):
+        #         print(location["lat"])
 
+    # print(return_data)
+    # print(return_data["results"])
+
+test_OK_google_places()
     
