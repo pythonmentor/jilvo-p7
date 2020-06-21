9a4aa447dff65de073881efe7f5caf32367dfaa7
unknown <jmillot41@gmail.com>
2020-06-21 17:09:59 +0200

    pytest parsing

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,14 +1,11 @@
-from pytest import *
 import sys
 import os
-# sys.path.insert(0, '../flask_app/')
-# from parse_question import parsing
 sys.path.append(f"{os.getcwd()}/flask_app/")
 from parse_question import parsing
-
 
 def test_parsing():
     results = ['tour', 'Eiffel', '']
     assert parsing("Où est la tour Eiffel ? ") == results
     
+
 
