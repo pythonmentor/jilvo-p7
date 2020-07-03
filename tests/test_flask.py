112b75ef0f569a5816dce73a3081023efb1a00fc
Jilvo <55281440+Jilvo@users.noreply.github.com>
2020-07-03 15:22:42 +0200

    add a third test

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -29,26 +29,55 @@ def test_request_google(monkeypatch):
     dict_google = object_from_import_class.search_by_google()
     assert dict_google == dico_grandpy_google
 
-def test_request_wiki(monkeypatch):
-    dico_grandpy_wiki = {'pageid' : '1359783', 'title' : 'Tour Eiffel'}
+# def test_request_wiki(monkeypatch):
+#     dico_grandpy_wiki = {'pageid' : '1359783', 'title' : 'Tour Eiffel'}
 
-    class MockRequestsGet_Wiki:
+#     class MockRequestsGet_Wiki:
+#         def __init__(self, url, params=None):
+#             self.status_code = 200
+#             self.dict_test_wiki = {}
+#         def json(self):
+#             self.dict_test_wiki = {
+#                 'query' : {
+#                     'geometry' : [{
+#                             'pageid' : '1359783',
+#                             'title' : 'Tour Eiffel',
+#                             'lat' : '48.8583',
+#                             'lon' : '2.2944813'
+
+#                     }]
+#                 }
+#             }
+#             return self.dict_test_wiki
+#     monkeypatch.setattr('requests.get', MockRequestsGet_Wiki)
+
+#     object_from_import_class = req_grandpy()
+#     dict_wiki = object_from_import_class.search_by_wiki()
+#     assert dict_wiki == dico_grandpy_wiki
+
+def test_request_wiki_bio(monkeypatch):
+    dico_grandpy_wiki_bio = {"pageid": 9232, "extract": "Paris is a city"}
+
+    class MockRequestsGet_Wiki_Bio:
         def __init__(self, url, params=None):
             self.status_code = 200
-            self.dict_test = {}
+            self.dict_test_wiki_bio = {}
         def json(self):
-            self.dict_test = {
-                'query' : {
-                    'geometry' : [{
-                            'pageid' : '1359783',
-                            'title' : 'Tour Eiffel'
-
-                    }]
+            self.dict_test_wiki_bio = {
+                "query": {
+                    "pages": {
+                        "9232": {
+                            "pageid": 9232,
+                            "extract": "Paris is a city"
+                        
+                        }
+                    }
                 }
             }
-            return self.dict_test
-    monkeypatch.setattr('requests.get', MockRequestsGet_Wiki)
+            return self.dict_test_wiki_bio
+    monkeypatch.setattr('requests.get', MockRequestsGet_Wiki_Bio)
 
     object_from_import_class = req_grandpy()
-    dict_google = object_from_import_class.search_by_wiki()
-    assert dict_google == dico_grandpy_wiki
+    dict_wiki_bio = object_from_import_class.search_by_wiki_bio()
+    assert dict_wiki_bio == dico_grandpy_wiki_bio
+
